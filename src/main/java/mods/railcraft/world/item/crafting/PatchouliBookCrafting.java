54d7ca96f4f7eef1b8c4f8750eac327533100ad4
Davide Albiero <davide2910@live.it>
2024-07-28 19:10:05 +0200

    Update deps and fix patchouli

--- src/main/java/mods/railcraft/world/item/crafting/PatchouliBookCrafting.java
+++ src/main/java/mods/railcraft/world/item/crafting/PatchouliBookCrafting.java
@@ -1,11 +1,8 @@
 package mods.railcraft.world.item.crafting;
 
-import org.jetbrains.annotations.Nullable;
+import mods.railcraft.api.core.RailcraftConstants;
 import mods.railcraft.world.item.RailcraftItems;
 import net.minecraft.core.NonNullList;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.CraftingBookCategory;
@@ -16,30 +13,13 @@ import vazkii.patchouli.api.PatchouliAPI;
 
 public class PatchouliBookCrafting extends ShapelessRecipe {
 
-  private static final String NBT_KEY = "patchouli:book";
-  private static final String NBT_VAL = "railcraft:guide_book";
-  @Nullable
-  private static Item GUIDE_BOOK;
-
   public PatchouliBookCrafting(CraftingBookCategory category) {
     super("", category, makeGuideBook(), NonNullList.of(Ingredient.EMPTY,
         Ingredient.of(Items.BOOK), Ingredient.of(RailcraftItems.IRON_CROWBAR.get())));
   }
 
-  private static Item guide_book() {
-    if (GUIDE_BOOK == null) {
-      GUIDE_BOOK = BuiltInRegistries.ITEM
-          .get(ResourceLocation.fromNamespaceAndPath(PatchouliAPI.MOD_ID, "guide_book"));
-    }
-    return GUIDE_BOOK;
-  }
-
   private static ItemStack makeGuideBook() {
-    var book = new ItemStack(guide_book());
-    //FIXME
-    //var tag = book.getOrCreateTag();
-    //tag.putString(NBT_KEY, NBT_VAL);
-    return book;
+    return PatchouliAPI.get().getBookStack(RailcraftConstants.rl("guide_book"));
   }
 
   @Override
